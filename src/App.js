49144f1575aa2187993cabdc92305d1073b8fe19
henok26 <henokaniguse1931@gmail.com>
2023-12-11 06:20:50 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout/>} />
+        <Route path="invoices" element={<Invoices/>} />
+        <Route path="dashboard" element={<Dashboard/>} />
+        {/* Add routes for other pages here */}
+        <Route />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/shared/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Outlet} from "react-router-dom"
+
+export default function Layout() {
+  return (
+    <div>
+      <div>sidebar</div>
+      <div>header</div>
+      <div>{<Outlet/>}</div>
+    </div>
+  )
+}
